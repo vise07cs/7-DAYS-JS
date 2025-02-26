a6f165335cf7136ccde968e07696e88b6e9ecc50
vise07cs <vikram7cs@gmail.com>
2025-02-26 12:31:16 +0530

    day02

---
+++ day2.js
@@ -0,0 +1,16 @@
+// null and undefined in javascript and difference between them;
+
+// undefined
+
+// ex-1
+let a;
+console.log(a)   
+
+// not defined
+// console.log(b)
+
+
+let c=null;
+console.log(c)
+c=c+20;
+console.log(c)
