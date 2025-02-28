e18cf9125dacf9565d54ed5de27d85bf248c24e5
vise07cs <vikram7cs@gmail.com>
2025-02-28 12:39:35 +0530

    day3

--- day2.js
+++ day2.js
@@ -1,16 +1,27 @@
-// null and undefined in javascript and difference between them;
+// null and undefined and not defined in javascript and difference between them;
 
 // undefined
+
+// A variable is declared but has not been assigned any value.
+// JavaScript automatically assigns undefined to uninitialized variables.
 
 // ex-1
 let a;
 console.log(a)   
 
 // not defined
-// console.log(b)
+
+// A variable is not declared at all in the code.
+// Accessing an undeclared variable results in a ReferenceError.
+
+// ex-2 of not defined
+console.log(b)
+
+
+// null
+// null is an explicit assignment that represents "no value" or "empty."
+// It must be manually assigned by the developer.
 
 
 let c=null;
-console.log(c)
-c=c+20;
 console.log(c)
---
+++ day3.js
@@ -0,0 +1,9 @@
+// Do you understand DOM , can you define whats the difference between event bubbling and event capturing 
+
+// Event Bubbling
+
+// Event bubbling is a concept in the DOM (Document Object Model) event propagation where an event starts from the target element (where the event occurred) and propagates upwards through its parent elements, up to the root (document).
+// if the child element doesnt have any event associated and parent element has an event , then also event will be triggered when an event is triggered on parent element
+
+
+
